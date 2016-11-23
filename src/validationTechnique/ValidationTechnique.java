915a74571df25553e1351257e01df22dbcd87993
Administrateur <khalil.kouiss@gmail.com>
2016-11-23 12:50:58 +0100

    First Commit

---
+++ src/validationTechnique/ValidationTechnique.java
@@ -0,0 +1,21 @@
+package validationTechnique;
+
+public class ValidationTechnique {
+
+	
+	public int num;
+	public String nomPrenom;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getNomPrenom() {
+		return nomPrenom;
+	}
+	public void setNomPrenom(String nomPrenom) {
+		this.nomPrenom = nomPrenom;
+	}
+	
+}
